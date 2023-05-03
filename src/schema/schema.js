464ed936e8b8531c74d49dd154cb01435964f602
BRITHE <bthellier51@gmail.com>
2023-05-03 17:41:53 +0200

    ✨ feat(schema.js): add Post type and createPost mutation. Add users, user, posts and post queries.

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -13,12 +13,32 @@ export const typeDefs = gql`
 		user: User!
 	}
 
+	type Post {
+		id: ID!
+		title: String!
+		content: String!
+		published: Boolean!
+		authorId: ID!
+		author: User!
+	}
+
+	input CreatePostInput {
+		title: String!
+		content: String!
+		published: Boolean!
+	}
+
 	type Query {
 		me: User
+		users: [User!]!
+		user(id: ID!): User!
+		posts: [Post!]!
+		post(id: ID!): Post!
 	}
 
 	type Mutation {
 		login(username: String!, password: String!): AuthPayload
 		signup(username: String!, email: String!, password: String!): AuthPayload
+		createPost(input: CreatePostInput!): Post!
 	}
 `;
